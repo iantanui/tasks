1b136ba318a1133584233a4afb801a964009674e
iantanui <rutooian@gmail.com>
2024-02-16 11:01:03 +0300

    create UI

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -1,14 +1,30 @@
 package com.example.tasks
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tasks.ui.theme.TasksTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,9 +44,65 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskApp() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Tasks") },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        // Icon for google profile
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /* Handle add task*/ },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add"
+                )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        content = {
+            TasksList()
+        }
+    )
+}
 
+@Composable
+fun TasksList() {
+    val tasks = listOf("Task 1", "Task 2", "Task 3")
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        items(tasks.size) { index ->
+            TaskItem(taskName = tasks[index])
+        }
+    }
+}
+
+@Composable
+fun TaskItem(taskName: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = taskName,
+            modifier = Modifier.padding(16.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
 }
 
 @Preview(showBackground = true)
