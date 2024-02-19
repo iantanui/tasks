84ff2e8ad896dd053bd3fd7a25e9e9cdc836c631
iantanui <rutooian@gmail.com>
2024-02-19 12:25:24 +0300

    add divider

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -32,6 +33,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tasks.ui.theme.TasksTheme
@@ -83,6 +85,7 @@ fun TaskApp() {
                         }
                     }
                 )
+                Divider(color = Color.Gray, thickness = 1.dp) // Divider line
 
                 TasksList(tasks = tasks)
             }
