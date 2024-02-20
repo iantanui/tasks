0acc9eca29b6ae33bf2d71494c3522a75e5a8cd9
iantanui <rutooian@gmail.com>
2024-02-20 13:12:57 +0300

    add clickable for completed

--- app/src/main/java/com/example/tasks/MainActivity.kt
+++ app/src/main/java/com/example/tasks/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -69,7 +70,7 @@ class MainActivity : ComponentActivity() {
 fun TaskApp() {
     var isAddTaskDialogOpen by remember { mutableStateOf(false) }
     var completedTasks by remember { mutableStateOf(listOf("helo", "jhhdgyg")) }
-    var incompleteTasks by remember { mutableStateOf(listOf<String>()) }
+    var incompleteTasks by remember { mutableStateOf(listOf("1st", "2nd")) }
 
     Scaffold(
         topBar = {
@@ -97,9 +98,6 @@ fun TaskApp() {
 
                 Divider(color = Color.Gray, thickness = 1.dp) // Divider line
 
-                TasksList(
-                    tasks = incompleteTasks,
-                    onTaskCompleted = { /* Implement task completion logic */ })
             }
         },
         floatingActionButton = {
@@ -118,33 +116,41 @@ fun TaskApp() {
         },
         floatingActionButtonPosition = FabPosition.End,
         content = {
-            if (isAddTaskDialogOpen) {
-                AddTaskDialog(
-                    onTaskAdded = { newTask ->
-                        incompleteTasks = listOf(newTask) + incompleteTasks
-                        isAddTaskDialogOpen = false
-                    },
-                    onDismiss = { isAddTaskDialogOpen = false }
+            Column(
+                modifier = Modifier
+                    .padding(top = 60.dp)
+            ) {
+                if (isAddTaskDialogOpen) {
+                    AddTaskDialog(
+                        onTaskAdded = { newTask ->
+                            incompleteTasks = listOf(newTask) + incompleteTasks
+                            isAddTaskDialogOpen = false
+                        },
+                        onDismiss = { isAddTaskDialogOpen = false }
 
-                )
-            }
-            // Display incomplete
-            TasksList(tasks = incompleteTasks, onTaskCompleted = { taskName ->
-                val taskToMove = incompleteTasks.find { it == taskName }
-                if (taskToMove != null) {
-                    incompleteTasks = incompleteTasks - taskToMove
-                    completedTasks = listOf(taskName) + completedTasks
-                }
-            })
-
-            if (completedTasks.isNotEmpty()) {
-                Column {
-                    Text(
-                        text = "Completed Tasks",
-                        style = MaterialTheme.typography.displayMedium,
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
                     )
-                    TasksList(tasks = completedTasks, onTaskCompleted = {})
+                }
+
+                // Display incomplete
+                TasksList(tasks = incompleteTasks, onTaskCompleted = { taskName ->
+                    // Move completed tasks to completedTasks list
+                    val taskToMove = incompleteTasks.find { it == taskName }
+                    if (taskToMove != null) {
+                        incompleteTasks = incompleteTasks - taskToMove
+                        completedTasks = listOf(taskName) + completedTasks
+                    }
+                })
+
+                // Display completed tasks section only if there are completed tasks available
+                if (completedTasks.isNotEmpty()) {
+                    Column {
+                        Text(
+                            text = "Completed Tasks",
+                            style = MaterialTheme.typography.displayMedium,
+                            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                        )
+                        TasksList(tasks = completedTasks, onTaskCompleted = {})
+                    }
                 }
             }
         }
@@ -191,14 +197,15 @@ fun TasksList(tasks: List<String>, onTaskCompleted: (String) -> Unit) {
         items(tasks.size) { index ->
             TaskItem(
                 taskName = tasks[index],
-                onTaskCompleted = onTaskCompleted
+                isCompleted = false,
+                onTaskClicked = { onTaskCompleted(tasks[index]) }
             )
         }
     }
 }
 
 @Composable
-fun TaskItem(taskName: String, onTaskCompleted: (String) -> Unit) {
+fun TaskItem(taskName: String, isCompleted: Boolean, onTaskClicked: () -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -209,14 +216,20 @@ fun TaskItem(taskName: String, onTaskCompleted: (String) -> Unit) {
                 .padding(8.dp),
             verticalAlignment = Alignment.CenterVertically,
         ) {
-
-            Box(
-                modifier = Modifier
-                    .clickable { onTaskCompleted(taskName) }
-                    .size(20.dp)
-                    .background(color = Color.Transparent, shape = CircleShape)
-                    .border(width = 1.dp, color = Color.Gray, shape = CircleShape)
-            )
+            if (isCompleted) {
+                Icon(
+                    imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "Completed"
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .clickable { onTaskClicked() }
+                        .size(20.dp)
+                        .background(color = Color.Transparent, shape = CircleShape)
+                        .border(width = 1.dp, color = Color.Gray, shape = CircleShape)
+                )
+            }
 
             Text(
                 text = taskName,
@@ -226,7 +239,6 @@ fun TaskItem(taskName: String, onTaskCompleted: (String) -> Unit) {
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
